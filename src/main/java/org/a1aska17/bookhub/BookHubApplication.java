4af82332df47923258902373bc979fa9c0bbd93f
a1aska17 <92105945+aLASKA17@users.noreply.github.com>
2026-08-07 00:24:54 +0600

    Create project

---
+++ src/main/java/org/a1aska17/bookhub/BookHubApplication.java
@@ -0,0 +1,13 @@
+package org.a1aska17.bookhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/a1aska17/bookhub/BookHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.a1aska17.bookhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
